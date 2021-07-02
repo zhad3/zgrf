2316c509d9e054476f1b7204f6de56cba3f7dc50
zhad3 <dragoonzombie@gmail.com>
2021-07-02 15:38:59 +0200

    Fix zlib uncompression
    The standard library throws when zlib returns Z_BUF_ERROR.
    However as per official documentation this is not an error.
    Rather is states that there is no more input being written.
    This for example happens when the compressed size is equal to the
    uncompressed size.
    
    This commit treats Z_BUF_ERROR the same as Z_STREAM_END.

--- compression/source/zgrf/compression.d
+++ compression/source/zgrf/compression.d
@@ -65,9 +65,62 @@ ubyte[] uncompress(const(ubyte)[] srcbuf, size_t destlen = 0u)
     }
     else
     {
-        import std.zlib : zlib_uncompress = uncompress;
-
         dst = cast(ubyte[]) zlib_uncompress(srcbuf, destlen);
     }
     return dst;
+}
+
+// Taken directly from std/zlib.d
+// This implementation does not treat Z_BUF_ERROR as an error that should be thrown
+private void[] zlib_uncompress(const(void)[] srcbuf, size_t destlen = 0u, int winbits = 15)
+{
+    import etc.c.zlib;
+    import std.zlib : ZlibException;
+    import std.conv : to;
+    int err;
+    ubyte[] destbuf;
+
+    if (!destlen)
+        destlen = srcbuf.length * 2 + 1;
+
+    etc.c.zlib.z_stream zs;
+    zs.next_in = cast(typeof(zs.next_in)) srcbuf.ptr;
+    zs.avail_in = to!uint(srcbuf.length);
+    err = etc.c.zlib.inflateInit2(&zs, winbits);
+    if (err)
+    {
+        throw new ZlibException(err);
+    }
+
+    size_t olddestlen = 0u;
+
+    loop:
+    while (true)
+    {
+        destbuf.length = destlen;
+        zs.next_out = cast(typeof(zs.next_out)) &destbuf[olddestlen];
+        zs.avail_out = to!uint(destlen - olddestlen);
+        olddestlen = destlen;
+
+        err = etc.c.zlib.inflate(&zs, Z_NO_FLUSH);
+        switch (err)
+        {
+            case Z_OK:
+                destlen = destbuf.length * 2;
+                continue loop;
+
+            case Z_BUF_ERROR:
+            case Z_STREAM_END:
+                destbuf.length = zs.total_out;
+                err = etc.c.zlib.inflateEnd(&zs);
+                if (err != Z_OK)
+                    throw new ZlibException(err);
+                return destbuf;
+
+            default:
+                etc.c.zlib.inflateEnd(&zs);
+                throw new ZlibException(err);
+        }
+    }
+    assert(0, "Unreachable code");
 }
